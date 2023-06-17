1cb4437d542d4fa14d00f15318c1d6fa466ac5dc
Alan Coiradas <coiradasalan@gmail.com>
2023-06-17 16:31:27 -0300

    se crearon los archivos correspondientes a los componentes a utilizar en el proyecto así como la primera función del boton

---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import '../component-styles/button.css'
+function Button (message, clickscounter, action ){
+    return(
+        <button className={clickscounter ? 'click' : 'reboot'} onClick={action}>
+            {message}
+        </button>
+
+    )
+
+}
+export default Button
